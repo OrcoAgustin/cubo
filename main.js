f844e9dc3b8e96a767efda55882d892948dad87d
Agustin <agustinorco@gmail.com>
2024-09-09 12:46:04 -0300

    se crea proyecto

---
+++ main.js
@@ -0,0 +1,22 @@
+let respuestas = [
+  "Reinicia el equipo",
+  "Demora en impactar el cambio, espera 5 minutos",
+  "Envia a soporte una captura del error",
+  "Es un problema general",
+  "Lo estamos viendo...",
+  "Es un problema en el server",
+];
+function conseguirRespuestas() {
+  let i = -1;
+  while (i < 0 || i > 5) {
+    i = Math.trunc(Math.random() * 10);
+  }
+  console.log(i, respuestas[i]);
+  return i;
+}
+
+$("#boton").click(function () {
+  let i = conseguirRespuestas();
+  $("#respuesta").html(respuestas[i]);
+  $("#imagen").attr("src", `img/i${i}.jpg`);
+});
